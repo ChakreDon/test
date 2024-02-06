126f650c1f48f980ab97b411e235347a08bcddef
Sudarshan <twayana44@gmail.com>
2024-02-06 12:54:17 +0000

    first message

--- all_in_docker/client/src/App.js
+++ all_in_docker/client/src/App.js
@@ -110,7 +110,7 @@ function App() {
             setWage(event.target.value);
           }}
         />
-        <button onClick={addEmployee}>Addition1 Employee</button>
+        <button onClick={addEmployee}>Addition Employee</button>
       </div>
       <div className="employees">
         <button onClick={getEmployees}>Show Employees</button>
